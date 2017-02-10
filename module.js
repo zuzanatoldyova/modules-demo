516ef61bc2c89adaf03eb3d3f201db99a8d8ed03
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-10 19:57:09 +0000

    added multiple function

--- module.js
+++ module.js
@@ -1,6 +1,11 @@
+var multiple = function(a, b){
+  return a * b;
+}
+
 function sum(a,b){
   return a + b;
 }
 
 
 exports.functionSum = sum;
+exports.functionMultiple = multiple;
--- my-script.js
+++ my-script.js
@@ -1,3 +1,4 @@
 var module = require('./module.js');
 
 console.log(module.functionSum(4,5));
+console.log(module.functionMultiple(2,3));
